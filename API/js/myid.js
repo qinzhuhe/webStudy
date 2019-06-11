6d115fc3870e45df78d5fad3a2dc24437c7bd521
qinzhuhe <2861969038@qq.com>
2019-06-12 00:12:46 +0800

    DOM学习--6.12(补充)

--- API/js/myid.js
+++ API/js/myid.js
@@ -73,3 +73,15 @@ function mouseroverHandle(){
 function mouseroutHandle(){
 	this.style.backgroundColor="";
 }
+
+//为任意元素.绑定任意的事件，任意的元素，时间的类型，事件处理函数
+function addEventListener(element,type,fn){
+	// 判断浏览器是否支持这个方法
+	if(element.addEventListener){
+		element.addEventListener(type,fn,false);
+	}else if(element.attachEvent){
+		element.attachEvent("on"+type,fn);
+	}else{
+		element["on"+type]=fn;
+	}
+}
